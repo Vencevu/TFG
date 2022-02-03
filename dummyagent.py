41f2448e7d77603464d07abcf5ea1f43f8b3bf01
Vencevu <albertovencevu@gmail.com>
2022-02-03 21:14:20 +0100

    agente tonto

--- dummyagent.py
+++ dummyagent.py
@@ -4,7 +4,7 @@ class DummyAgent(agent.Agent):
     async def setup(self):
         print("Hello World! I'm agent {}".format(str(self.jid)))
 
-dummy = DummyAgent("agente@DESKTOP-OUIVPSP", "1234")
+dummy = DummyAgent("agente@localhost", "1234")
 future = dummy.start()
 future.result()
 
