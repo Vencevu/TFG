d436596f3d137457b45fcae8c1d1bdfb8a79b357
Alberto <alcargra@outlook.es>
2022-02-09 20:03:40 +0100

    ok

--- dummyagent.py
+++ dummyagent.py
@@ -1,8 +1,10 @@
 from spade import agent, quit_spade
 
+
 class DummyAgent(agent.Agent):
     async def setup(self):
         print("Hello World! I'm agent {}".format(str(self.jid)))
+
 
 dummy = DummyAgent("agente@localhost", "1234")
 future = dummy.start()
