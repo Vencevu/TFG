05f6af57fc92ee4ba61d436c0a894b022a5f91c9
Vencevu <albertovencevu@gmail.com>
2022-02-12 16:03:54 +0100

    agente conecta con simulador

--- dummyagent.py
+++ dummyagent.py
@@ -4,13 +4,19 @@ import carla
 #Clase Agente
 class DummyAgent(agent.Agent):
     async def setup(self):
+        #Conexion con CARLA
+        client = carla.Client('127.0.0.1', 2000)
+        client.set_timeout(10.0)
+
+        world = client.get_world()
+
+        spawn_points = world.get_map().get_spawn_points()
+        number_of_spawn_points = len(spawn_points)
+        print(number_of_spawn_points)
+
         print("Hello World! I'm agent {}".format(str(self.jid)))
 
-#Conexion con CARLA
-client = carla.Client('127.0.0.1', 2000)
-client.set_timeout(10.0)
 
-world = client.get_world()
 
 #Lanzamos el agente
 dummy = DummyAgent("agente@localhost", "1234")
