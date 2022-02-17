ed8435b7395df55a49f8247652a7d73752be417a
Vencevu <albertovencevu@gmail.com>
2022-02-17 16:30:02 +0100

    ok

--- dummyagent.py
+++ dummyagent.py
@@ -17,14 +17,16 @@ class DummyAgent(agent.Agent):
         #Configuracion del mundo
         world = client.get_world()
 
-        #Creamos vehiculo y sensor
+        #Creamos vehiculo
         actor_list = []
         blueprint_library = world.get_blueprint_library()
         bp = random.choice(blueprint_library.filter('vehicle'))
         transform = random.choice(world.get_map().get_spawn_points()) 
         vehicle = world.spawn_actor(bp, transform) 
+        vehicle.set_autopilot(True)
         actor_list.append(vehicle)
 
+        #Creamos sensor y lo acoplamos al vehiculo
         camera_depth = blueprint_library.find('sensor.camera.depth')
         camera_transform = carla.Transform(carla.Location(x=0.8, z=1.7))
         camera_d = world.spawn_actor(camera_depth, camera_transform, attach_to=vehicle)
@@ -32,15 +34,17 @@ class DummyAgent(agent.Agent):
         camera_d.listen(image_queue_depth.put)
         actor_list.append(camera_d)
 
+        #Guardamos informacion del sensor
         image_depth = image_queue_depth.get()
         image_depth.save_to_disk("test_images/%06d_depth.png" %(image_depth.frame), carla.ColorConverter.LogarithmicDepth)
-
-        client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
-        print("Hello World! I'm agent {}".format(str(self.jid)))
+        print(image_depth.raw_data)
+        #client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
+        
 
 #Lanzamos el agente
 dummy = DummyAgent("agente@localhost", "1234")
 future = dummy.start()
+#Esperamos a que termine el setup
 future.result()
 
 dummy.stop()
