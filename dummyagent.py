9050e9af5179ba3d15c8be3f7c5ccea9e4441b9c
Alberto <alcargra@outlook.es>
2022-02-23 17:47:10 +0100

    ok

--- dummyagent.py
+++ dummyagent.py
@@ -1,10 +1,8 @@
 from spade import agent, quit_spade
 import carla
 import random
-import queue
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import time
+import open3d as o3d
 
 #Clase Agente
 class DummyAgent(agent.Agent):
@@ -22,7 +20,6 @@ class DummyAgent(agent.Agent):
         self.world = self.client.get_world()
 
         #Creamos vehiculo
-        
         blueprint_library = self.world.get_blueprint_library()
         bp = random.choice(blueprint_library.filter('vehicle'))
         transform = random.choice(self.world.get_map().get_spawn_points()) 
@@ -30,16 +27,21 @@ class DummyAgent(agent.Agent):
         self.actor_list.append(vehicle)
 
         #Creamos sensor y lo acoplamos al vehiculo
-        camera_depth = blueprint_library.find('sensor.camera.depth')
-        camera_transform = carla.Transform(carla.Location(x=0.8, z=1.7))
-        camera_d = self.world.spawn_actor(camera_depth, camera_transform, attach_to=vehicle)
-        image_queue_depth = queue.Queue()
-        camera_d.listen(image_queue_depth.put)
-        self.actor_list.append(camera_d)
+        lidar_cam = None
+        lidar_bp = self.world.get_blueprint_library().find('sensor.lidar.ray_cast')
+        lidar_bp.set_attribute('channels',str(32))
+        lidar_bp.set_attribute('points_per_second',str(90000))
+        lidar_bp.set_attribute('rotation_frequency',str(40))
+        lidar_bp.set_attribute('range',str(20))
+        lidar_location = carla.Location(0,0,2)
+        lidar_rotation = carla.Rotation(0,0,0)
+        lidar_transform = carla.Transform(lidar_location,lidar_rotation)
+        lidar_sen = self.world.spawn_actor(lidar_bp,lidar_transform,attach_to=vehicle)
+        lidar_sen.listen(lambda point_cloud: point_cloud.save_to_disk('test_images/%.6d.ply' % point_cloud.frame))
 
-        #Guardamos informacion del sensor
-        image_depth = image_queue_depth.get()
-        image_depth.save_to_disk("test_images/%06d_depth.png" %(image_depth.frame), carla.ColorConverter.LogarithmicDepth)
+        self.actor_list.append(lidar_sen)
+
+
         self.client.apply_batch([carla.command.DestroyActor(x) for x in self.actor_list])
         
 
