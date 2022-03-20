6a1d4552c3178a8da0ff268fdadc1a9de0fbb208
Vencevu <albertovencevu@gmail.com>
2022-03-20 10:53:43 +0100

    ok

--- DQNTest.py
+++ DQNTest.py
@@ -1,5 +1,7 @@
-import numpy as np
+import time
+import Config
 import random
+import numpy as np
 import tensorflow as tf
 from collections import deque
 from keras.models import Model
@@ -7,10 +9,22 @@ from keras.callbacks import TensorBoard
 from keras.applications.xception import Xception
 from keras.layers import Dense, GlobalAveragePooling2D, Flatten
 
-
 class DQNAgent:
     def __init__(self):
+        self.model = self.create_model()
 
-        random.seed(1)
-        np.random.seed(1)
-        tf.random.set_seed(1)
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        
+    def create_model(self):
+        ## input: RGB data, should be normalized when coming into CNN
+        base_model = Xception(weights=None, include_top=False, input_shape=(Config.IM_HEIGHT, Config.IM_WIDTH, 3))
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Flatten()(x)
+
+        predictions = Dense(3, activation="linear")(x)  ## output layer include three nuros, representing three actions
+        model = Model(inputs=base_model.input, outputs=predictions)
+        model.compile(loss="mse", optimizer='adam', metrics=["accuracy"])
+        return model
