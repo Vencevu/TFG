9242ec8980d643dca87c781ee66ae55f5963e102
Vencevu <albertovencevu@gmail.com>
2022-03-21 15:59:50 +0100

    ok

--- CarlaEnv.py
+++ CarlaEnv.py
@@ -1,8 +1,13 @@
 import carla
+import math
+import Config
 
 class CarlaEnv:
 
     def __init__(self):
+        self.reward = Config.MIN_REWARD
+        self.done = False
+        
         self.actor_list = []
         self.client = carla.Client('127.0.0.1', 2000)
         self.client.set_timeout(2.0)
@@ -23,10 +28,21 @@ class CarlaEnv:
         self.vehicle = self.world.spawn_actor(bp, transform) 
         self.actor_list.append(self.vehicle)
 
-    def car_step(self, action):
+    def vehicle_velocity(self):
+        v = self.vehicle.get_velocity()
+        kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+        return kmh
+
+    def vehicle_step(self, action):
         if action == 0:
             self.car_velocity = 0
         if action == 1:
             self.car_velocity = 10
         
-        return self.car_velocity
+        vel = self.vehicle_velocity()
+        if(vel < 10 or vel > 30):
+            self.reward = Config.MIN_REWARD
+        else:
+            self.reward = Config.MIN_REWARD * 3
+        
+        return self.car_velocity, self.reward, self.done
