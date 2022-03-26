2909d0b69e8df4b7c53f88313597756a49295904
Vencevu <albertovencevu@gmail.com>
2022-03-26 21:47:21 +0100

    ok

--- v1/CarlaEnv.py
+++ v1/CarlaEnv.py
@@ -47,10 +47,11 @@ class CarlaEnv:
             sensor = self.world.try_spawn_actor(bp, transform, attach_to=self.vehicle)
             self.actor_list.append(sensor)
             sensor.listen(lambda data: self.process_img(data))
-        elif sensor == "obstacle_detector":
+        
+        if sensor == "obstacle_detector":
             bp = self.blueprint_library.find('sensor.other.obstacle')
             bp.set_attribute('sensor_tick', '1.0')
-            
+
             transform = carla.Transform(carla.Location(x=0.8, z=1.7))
             sensor = self.world.try_spawn_actor(bp, transform, attach_to=self.vehicle)
             self.actor_list.append(sensor)
