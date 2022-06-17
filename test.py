3a2004c7f26cd0d8937d8012ea426502ad76e1f2
Vencevu <albertovencevu@gmail.com>
2022-06-17 18:13:26 +0200

    ok

--- test.py
+++ test.py
@@ -1,7 +1,39 @@
-import matplotlib.pyplot as plt
+from spade import agent, quit_spade
+from spade.behaviour import CyclicBehaviour
+import time
 
-plt.plot([1,2,3,4,5], [1,1,2,3,2])
-plt.savefig("1.png")
-plt.clf()
-plt.scatter([1,2,3,4,5], [1,1,2,3,2])
-plt.savefig("2.png")
+class CarAgent(agent.Agent):
+
+    #Comportamiento del agente
+    class MyBehav(CyclicBehaviour):
+        current_state = None
+    
+        async def on_start(self):
+            print("Comportamiento iniciado")
+
+        async def run(self):
+            print("Ejecucion")
+
+        async def on_end(self):
+            print("Behaviour finished with exit code {}.".format(self.exit_code))
+    
+    async def setup(self):
+        self.my_behav = self.MyBehav()
+        self.add_behaviour(self.my_behav)
+
+
+#Lanzamos el agente
+dummy = CarAgent("agente@localhost", "1234")
+future = dummy.start()
+future.result()
+
+while not dummy.is_alive():
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        dummy.stop()
+        break
+
+print("Agent %s finished" % dummy.name)
+
+quit_spade()
