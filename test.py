9d59bfbf453868384d7547d8a96cf4ce8695103b
Vencevu <albertovencevu@gmail.com>
2022-06-25 23:21:45 +0200

    ok

--- test.py
+++ test.py
@@ -2,40 +2,24 @@ import csv
 from decimal import Decimal
 import matplotlib.pyplot as plt
 
-dist_csv = open('csv/validate/Distances.csv', 'r')
+dist_csv = open('csv/validate/Acc1.csv', 'r')
 data = csv.reader(dist_csv,delimiter=';')
 
-x_axis_time = []
-x_axis_goal = []
-x_axis_col = []
-y_axis_time = []
-y_axis_col = []
-y_axis_goal = []
+x=[]
+y=[]
 
 for row in data:
     d = row
-    
-    num = float(d[1].replace(',', '.'))
-    num = float(num)
+    num = float(d[1])
 
-    if d[2] == 'Tiempo':
-        y_axis_time.append(num)
-        x_axis_time.append(int(d[0]))
-    elif d[2] == 'Colision':
-        y_axis_col.append(num)
-        x_axis_col.append(int(d[0]))
-    elif d[2] == 'Objetivo':
-        y_axis_goal.append(num)
-        x_axis_goal.append(int(d[0]))
+    y.append(num)
+    x.append(int(d[0]))
     
 
 
-plt.scatter(x_axis_time, y_axis_time, label="time reset")
-plt.scatter(x_axis_col, y_axis_col, label="collision reset")
-plt.scatter(x_axis_goal, y_axis_goal, label="goal")
-plt.legend(loc="upper left")
+plt.plot(x, y)
 plt.xlabel("Episodios")
-plt.ylabel("Distancia al objetivo")
-plt.savefig('Distancias.png')
+plt.ylabel("Precisión")
+plt.savefig('Acc.png')
 
 dist_csv.close()
