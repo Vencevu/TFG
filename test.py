51455ae6b12966c6d0a8035beb3ce03d448216d6
Vencevu <albertovencevu@gmail.com>
2022-06-26 11:41:01 +0200

    ok

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import csv
 from decimal import Decimal
 import matplotlib.pyplot as plt
 
-dist_csv = open('csv/validate/Acc1.csv', 'r')
+dist_csv = open('csv/validate/Loss1.csv', 'r')
 data = csv.reader(dist_csv,delimiter=';')
 
 x=[]
@@ -10,16 +10,19 @@ y=[]
 
 for row in data:
     d = row
-    num = float(d[1])
+    num = d[1]
 
+    num = num.replace('.', '', num.count('.') - 1)
+    num = float(num)
     y.append(num)
     x.append(int(d[0]))
     
 
 
 plt.plot(x, y)
+plt.yscale('log')
 plt.xlabel("Episodios")
-plt.ylabel("Precisión")
-plt.savefig('Acc.png')
+plt.ylabel("Pérdida")
+plt.savefig('Loss.png')
 
 dist_csv.close()
